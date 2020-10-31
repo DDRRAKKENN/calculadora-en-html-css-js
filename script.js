c88919acc657ae614d2fa14b26543a0eeeee3467
Jorge Ignacio Lara Ceballos <zero19b@hotmail.com>
2020-10-31 11:26:27 -0300

    cambios en js y html

--- script.js
+++ script.js
@@ -5,7 +5,5 @@ function sumar(){
     valor2=document.getElementById("valor2").value;
     resultado=parseInt(valor1)+parseInt(valor2);
     
-    
-    console.log(`la suma de ${valor1} y el valor ${valor2} es ${resultado}`);
-    
+    document.getElementById("resultado").innerHTML=(`el resultado de la suma de ${valor1} y el valor ${valor2} es ${resultado}`);
 };
