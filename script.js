47fd8d6fbdf441691c4140265b2d5f8cb1d85459
Jorge Ignacio Lara Ceballos <zero19b@hotmail.com>
2020-10-31 11:21:31 -0300

    pasados desde la pc

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function sumar(){
     valor2=document.getElementById("valor2").value;
     resultado=parseInt(valor1)+parseInt(valor2);
     
+    
     console.log(`la suma de ${valor1} y el valor ${valor2} es ${resultado}`);
     
 };
