dd5690f9613dd1782830bd440338097a56b21dff
Jorge Ignacio Lara Ceballos <zero19b@hotmail.com>
2020-10-31 11:16:54 -0300

    nuevo unidad 2 (resubido)

---
+++ script.js
@@ -0,0 +1,10 @@
+function sumar(){
+    var valor1,valor2;  
+    
+    valor1=document.getElementById("valor1").value;
+    valor2=document.getElementById("valor2").value;
+    resultado=parseInt(valor1)+parseInt(valor2);
+    
+    console.log(`la suma de ${valor1} y el valor ${valor2} es ${resultado}`);
+    
+};
