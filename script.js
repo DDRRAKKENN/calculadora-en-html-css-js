47d2226421440987b590de1b5e73e4c7dfb6e749
Jorge Ignacio Lara Ceballos <zero19b@hotmail.com>
2020-11-01 22:26:24 -0300

    cambios referidos segun pedido de ejercicio

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ function sumar(){
     
     document.getElementById("resultado").innerHTML=(`El resultado de la suma de ${valor1} y el valor ${valor2} es ${res}`);
 };
+function restar(){
+    var valor1,valor2;  
+    
+    valor1=document.getElementById("valor1").value;
+    valor2=document.getElementById("valor2").value;
+    res=parseInt(valor1)-parseInt(valor2);
+    
+    document.getElementById("resultado").innerHTML=(`El resultado de la resta de ${valor1} y el valor ${valor2} es ${res}`);
+};
+function multiplicar(){
+    var valor1,valor2;  
+    
+    valor1=document.getElementById("valor1").value;
+    valor2=document.getElementById("valor2").value;
+    res=parseInt(valor1)*parseInt(valor2);
+    
+    document.getElementById("resultado").innerHTML=(`El resultado de la multiplicacion de ${valor1} y el valor ${valor2} es ${res}`);
+};
+function dividir(){
+    var valor1,valor2;  
+    
+    valor1=document.getElementById("valor1").value;
+    valor2=document.getElementById("valor2").value;
+    res=parseInt(valor1)/parseInt(valor2);
+    
+    document.getElementById("resultado").innerHTML=(`El resultado de dividir  ${valor1} y el valor ${valor2} es ${res}`);
+};
